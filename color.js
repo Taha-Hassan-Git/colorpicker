43b12fa4e630b563fccbff90798c0e5babd4c699
Taha Hassan <tahafhassan@gmai.com>
2022-12-27 22:15:57 +0000

    lock button adds locked to classlist, generate ignores locked elements

--- color.js
+++ color.js
@@ -1,11 +1,31 @@
-const generateBtn = document.getElementById("generate")
-function randomColor(element){
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    element.style.backgroundColor = "#" + randomColor;
-}
+const generateBtn = document.getElementById("generate");
+const lockBtnArray = document.querySelectorAll(".lock");
 
-document.querySelectorAll('.color').forEach(randomColor)
 
+lockBtnArray.forEach((element) => {
+    element.addEventListener("click", handleLock);
+})
 
 generateBtn.addEventListener("click", function () {
     document.querySelectorAll('.color').forEach(randomColor)});
+
+function randomColor(element){
+    if (element.classList.contains("locked")){
+
+    } else {
+        const colorName = element.querySelector('.name');
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        element.style.backgroundColor = "#" + randomColor;
+        colorName.innerHTML = "#" + randomColor;
+    }
+    
+}
+
+function handleLock(e){
+    const parentElement = e.target.parentNode.parentNode;
+    parentElement.classList.add("locked");
+}
+
+document.querySelectorAll('.color').forEach(randomColor);
+
+
